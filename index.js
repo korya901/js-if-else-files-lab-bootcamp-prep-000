9794bac93b8822aa4d05fbb5c7daea73feb4a2f3
Christen Williams <korya901@me.com>
2018-01-06 21:38:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var height = "70"
 if (name === "susan") { console.log("The name is correct")
 }
 
-if (height === 70) {console.log("The height is correct")
+if (height === "70") {console.log("The height is correct")
 }
 
 // Don't worry about this
